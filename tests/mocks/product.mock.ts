7141be25c197594a27345ef3b068206bf8190fe8
leocaridade <leocaridade@hotmail.com>
2023-08-11 16:02:41 -0400

    test: create unit test for function create on service layer

---
+++ tests/mocks/product.mock.ts
@@ -0,0 +1,12 @@
+import { Product } from '../../src/types/Product';
+
+const validProductFromDB: Product = {
+  id: 1,
+  name: 'Excalibur',
+  price: '10 peças de ouro',
+  orderId: 1,
+}
+
+export default {
+  validProductFromDB,
+};
--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,7 +1,19 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import ProductModel from '../../../src/database/models/product.model';
+import productService from '../../../src/services/product.service'
+import productMock from '../../mocks/product.mock';
 
 describe('ProductsService', function () {
   beforeEach(function () { sinon.restore(); });
 
+  it('deve ser possível criar um produto com sucesso', async function () {
+    const mockCreateReturn = ProductModel.build(productMock.validProductFromDB);
+    sinon.stub(ProductModel, 'create')
+      .resolves(mockCreateReturn);
+
+    const serviceResponse = await productService.create(productMock.validProductFromDB);
+
+    expect(serviceResponse).to.deep.eq(productMock.validProductFromDB);
+  });
 });
